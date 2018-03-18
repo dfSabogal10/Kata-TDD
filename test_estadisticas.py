721b678936fc0a1d631dffcb773ff144115678d9
dfSabogal10 <david.sabogal1@gmail.com>
2018-03-18 16:28:39 -0500

    Iteracion 1: Se crea el test para cuando la cadena son multiples numeros (rojo)

--- test_estadisticas.py
+++ test_estadisticas.py
@@ -3,11 +3,14 @@ from unittest import TestCase
 from Estadisticas import Estadisticas
 
 class TestEstadisticas(TestCase):
-    def test_estadisticas(self):
+    def test_estadisticas_it1(self):
         self.assertEqual(Estadisticas().estadisticas(""),[0],"cadena vacia")
 
-    def test_estadisticas_unnumero(self):
+    def test_estadisticas_it1_unnumero(self):
         self.assertEqual(Estadisticas().estadisticas("1"),[1],"Un numero")
 
-    def test_estadisticas_dosnumeros(self):
+    def test_estadisticas_it1_dosnumeros(self):
         self.assertEqual(Estadisticas().estadisticas("1,2"), [2], "Dos numeros")
+
+    def test_estadisticas_it1_multiplesnumeros(self):
+        self.assertEqual(Estadisticas().estadisticas("1,2,3,4"), [4], "Multiples numeros")
