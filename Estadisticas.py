26354b62282eea19dfcfddbb420b3efa2d36a71f
dfSabogal10 <david.sabogal1@gmail.com>
2018-03-18 16:58:11 -0500

    Iteracion 2: Se corrige el metodo estadisticas_it2 para que pase la prueba (verde)

--- Estadisticas.py
+++ Estadisticas.py
@@ -11,5 +11,11 @@ class Estadisticas:
     def estadisticas_it2(self,cadena):
         if cadena=="":
             return [0,None]
+        elif "," in cadena:
+            numeros=cadena.split(",")
+            if numeros[0]<numeros[1]:
+                return [2,int(numeros[0])]
+            else:
+                return [2,int(numeros[1])]
         else:
             return [1,int(cadena)]
